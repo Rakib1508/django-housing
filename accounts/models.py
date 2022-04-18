6046839b54434c917b52ffc9fa724c757e394bf6
rifatrakib <Rakib.1508@outlook.com>
2022-04-18 20:34:32 +0600

    modified: first and last name made compulsory, group removed, set preferred name if blank

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,17 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
 
 class Account(AbstractUser):
-    """Keep only the fields that are crucial for authentication and authorization
-    in this model as this will be kept on a separate database.
-    """
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    
-    # preferred name will be set to first name if blank
-    # TODO: handle this during saving the data to database
-    preferred_name = models.CharField(max_length=128, blank=True)
-    # Groups will be handled from the admin panel as it is not user accessible
-    role = models.ForeignKey(Group, on_delete=models.CASCADE)
+    first_name = models.CharField(_('First Name'))
+    last_name = models.CharField(_('Last Name'))
+    preferred_name = models.CharField(_('Preferred Name'), max_length=150, blank=True)
     
     def __str__(self):
         return self.username
+    
+    def set_preferred_name(self):
+        """preferred name will be set to first name if blank"""
+        if self.preferred_name is None:
+            self.preferred_name = self.first_name
