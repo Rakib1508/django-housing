cde71156a2542f96659e38f1327a9d3cc907191a
rifatrakib <Rakib.1508@outlook.com>
2022-04-27 20:32:33 +0600

    added json fields to store user profile information in profile model

--- profiles/models.py
+++ profiles/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
         _('name to display'), max_length=300,
         blank=True, null=True
     )
+    last_name_first = models.BooleanField(_('show last name first'), default=False)
     nicknames = ArrayField(
         models.CharField(max_length=50), blank=True, size=5,
         help_text=_('add up to 5 nicknames'),
@@ -16,4 +17,24 @@ class Profile(models.Model):
     short_bio = models.CharField(
         _('add a short introduction'),
         max_length=1000, blank=True, null=True
+    )
+    profile_tags = ArrayField(
+        models.CharField(max_length=50), blank=True, size=10,
+        help_text=_('add important tags so that others can find you easily'),
+    )
+    social_links = models.JSONField(
+        null=True, blank=True,
+        help_text=_('link your social world'),
+    )
+    places = models.JSONField(
+        null=True, blank=True,
+        help_text=_('places you have lived in'),
+    )
+    educations = models.JSONField(
+        null=True, blank=True,
+        help_text=_('educational institutes you have studied at'),
+    )
+    workplaces = models.JSONField(
+        null=True, blank=True,
+        help_text=_('companies you have worked at'),
     )
