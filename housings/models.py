8d04abb563898742b02d01f33167780be78c5df2
rifatrakib <Rakib.1508@outlook.com>
2022-05-11 02:14:19 +0600

    property model field added

--- housings/models.py
+++ housings/models.py
@@ -1,3 +1,9 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+    property_number = models.IntegerField(
+        _('property identification number'), unique=True,
+        help_text=_('identifier of each property'),
+    )
